68dbb628623bb6f16e4d9d9001c33f05bc5a3f2f
DicousDev <joaodicu387@gmail.com>
2025-09-03 13:32:17 -0300

    feat: adicionando mobile responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
   theme: {
     fontFamily: {
       'sans': ['Roboto', 'sans-serif']
+    },
+    extend: {
+      screens: {
+        'max-430': { 'max': '430px'},
+        'max-414': { 'max': '414px'},
+        'max-390': { 'max': '390px'},
+        'max-360': { 'max': '360px'},
+        'max-344': { 'max': '344px'}
+      }
     }
   },
   plugins: [],
